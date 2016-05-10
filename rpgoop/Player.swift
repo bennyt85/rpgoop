4eb09a8aa5698daed07edc8fed84f624701ef700
ben <benjamintalarico@gmail.com>
2016-05-10 16:53:29 -0500

    added player code

---
+++ rpgoop/Player.swift
@@ -0,0 +1,41 @@
+//
+//  Player.swift
+//  rpgoop
+//
+//  Created by admin on 5/9/16.
+//  Copyright © 2016 bentalarico. All rights reserved.
+//
+
+import Foundation
+
+
+class Player: Character {
+    
+    private var _name = "Player"
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    private var _inventory = [String] ()
+    
+    var inventory: [String] {
+        return _inventory
+    }
+    
+    func addItemToInventory(item: String) {
+        _inventory.append(item)
+    }
+    
+    
+    convenience init(name: String, hp: Int, attackPwr: Int) {
+        
+        self.init(startingHp: hp, attackPwr: attackPwr)
+        
+        _name = name 
+    }
+    
+}
+
